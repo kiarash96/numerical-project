b9c9153ad5a563f399158d697c3df152e389e183
Kiarash Korki <kiarash.96@gmail.com>
2016-12-11 18:49:24 +0330

    Implement other methods in chapter 2

--- app/src/java/project/app/ChapterTwoView.java
+++ app/src/java/project/app/ChapterTwoView.java
@@ -1,10 +1,14 @@
 package project.app;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import matlabcontrol.MatlabInvocationException;
+import project.LatexParser;
 import project.app.utility.MatlabConnection;
+import project.app.utility.MatlabStruct;
 import project.controls.AbusedTextField;
 import project.controls.LatexLabel;
 
@@ -12,6 +16,8 @@ import project.controls.LatexLabel;
  * Created by kiarash on 12/7/16.
  */
 public class ChapterTwoView {
+
+    LatexParser parser = new LatexParser();
 
     @FXML AbusedTextField fn1TextField, lowTextField, highTextField, steps1TextField;
     @FXML ComboBox<String> methodChoice;
@@ -30,7 +36,10 @@ public class ChapterTwoView {
         this.connection = connection;
 
         // First screen
-        latexLabel1.latexProperty().bind(fn1TextField.textProperty());
+        fn1TextField.textProperty().addListener(((observable, oldValue, newValue) -> {
+            latexLabel1.setLatex(parser.latex(newValue));
+        }));
+        fn1TextField.setText("saa");
 
         methodChoice.getItems().addAll("Bisection", "False Position", "Secant", "Newton-Raphson");
 
@@ -48,27 +57,37 @@ public class ChapterTwoView {
     }
 
     private void doType1(String method) {
-        /*Object[] res = connection.feval(method, 6,
-                fn1TextField.getText(),
-                Double.parseDouble(lowTextField.getText()),
-                Double.parseDouble(highTextField.getText()),
-                Double.parseDouble(steps1TextField.getText()),
-                0.01);
-        double[] highs = (double[]) res[0];
-        double[] lows = (double[]) res[1];
-        double[] roots = (double[]) res[2];
-        double[] values = (double[]) res[3];
-        String message = (String) res[4];
-        double[] fail = (double[]) res[5];
+        MatlabStruct args = new MatlabStruct(
+                new MatlabStruct.Pair<>("func", fn1TextField.getText()),
+                new MatlabStruct.Pair<>("low", Double.parseDouble(lowTextField.getText())),
+                new MatlabStruct.Pair<>("high", Double.parseDouble(highTextField.getText())),
+                new MatlabStruct.Pair<>("step", Double.parseDouble(steps1TextField.getText())),
+                new MatlabStruct.Pair<>("tol", 0.01)
+        );
 
-        if (fail[0] == 1)
+        MatlabStruct res = null;
+        try {
+            res = connection.feval("chapter-2", method, args,
+                    "highs", "lows", "roots", "values", "message", "fail");
+        } catch (MatlabInvocationException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+
+        double[] highs = res.get("highs");
+        double[] lows = res.get("lows");
+        double[] roots = res.get("roots");
+        double[] values = res.get("values");
+        String message = res.get("message");
+        double fail = res.get("fail");
+
+        if (fail == 1)
             ans1Label.setText(message);
         else {
             StringBuilder ans = new StringBuilder("");
             for (int i = 0; i < roots.length; i ++)
                 ans.append((i == 0 ? "" : " -> ") + roots[i]);
             ans1Label.setText(ans.toString());
-        }*/
+        }
     }
 
     private void doNewtonRaphson() {
