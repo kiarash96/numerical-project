7abb54fd07b31454b1b77faaee0b8089dc9c7be0
Kiarash Korki <kiarash.96@gmail.com>
2017-01-05 11:40:07 +0330

    Chapter 4 integral controller

--- app/src/java/project/app/ChapterFourController.java
+++ app/src/java/project/app/ChapterFourController.java
@@ -1,6 +1,17 @@
 package project.app;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import matlabcontrol.MatlabInvocationException;
+import project.LatexParser;
 import project.app.utility.MatlabConnection;
+import project.app.utility.MatlabStruct;
+import project.controls.LatexLabel;
 
 /**
  * Created by kiarash on 1/4/17.
@@ -8,8 +19,47 @@ import project.app.utility.MatlabConnection;
 public class ChapterFourController {
 
     MatlabConnection connection;
+    LatexParser parser;
+
+    // Integral
+    @FXML TextField intStartTextField, intEndTextField, intHTextField, intFunctionTextField;
+    @FXML ChoiceBox<String> intMethodType;
+    @FXML LatexLabel intFunctionLatexLabel, intAnswerLatexLabel;
+    @FXML Button intCalcButton;
+    @FXML ImageView intPlotView;
 
     public void init(MatlabConnection connection) {
         this.connection = connection;
+        parser = new LatexParser();
+
+        intMethodType.getItems().addAll("Trapezoidal", "Simpson 1/3", "Simpson 3/8",
+                "Romberg", "Gauss Legendre", "Customized Simpson");
+        intFunctionTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+            intFunctionLatexLabel.setLatex(parser.latex(newValue));
+        });
+        intCalcButton.setOnAction(event -> callIntegral());
+    }
+
+    public void callIntegral() {
+        MatlabStruct args = new MatlabStruct(
+                new MatlabStruct.Pair<>("f", intFunctionTextField.getText()),
+                new MatlabStruct.Pair<>("start", Double.parseDouble(intStartTextField.getText())),
+                new MatlabStruct.Pair<>("end", Double.parseDouble(intEndTextField.getText())),
+                new MatlabStruct.Pair<>("h", Double.parseDouble(intHTextField.getText())),
+                new MatlabStruct.Pair<>("method", intMethodType.getSelectionModel().getSelectedIndex()),
+                new MatlabStruct.Pair<>("plotPath", getClass().getResource("/matlab").getPath() + "/plot.jpg")
+        );
+        try {
+            MatlabStruct res = connection.feval("chapter-4", "integral", args, "ans");
+        } catch (MatlabInvocationException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+
+        intAnswerLatexLabel.setLatex(args.get("ans"));
+        intPlotView.setImage(new Image(this.getClass().getResourceAsStream("/matlab/plot.jpg")));
+    }
+
+    public void callDiff() {
+
     }
 }
---
+++ app/src/resources/matlab/chapter-4/integral.m
@@ -0,0 +1,3 @@
+function [ans] = integral(f, start, end, h, method, plotPath)
+
+end
