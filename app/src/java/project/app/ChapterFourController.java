a14c75f5b0f0e3a3c49361dc4f16a7676f6487fe
Kiarash Korki <kiarash.96@gmail.com>
2017-01-05 19:35:18 +0330

    use n for romberg and gauss legendre methods

--- app/src/java/project/app/ChapterFourController.java
+++ app/src/java/project/app/ChapterFourController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -22,6 +23,7 @@ public class ChapterFourController {
     LatexParser parser;
 
     // Integral
+    @FXML Label intHLabel;
     @FXML TextField intStartTextField, intEndTextField, intHTextField, intFunctionTextField;
     @FXML ChoiceBox<String> intMethodType;
     @FXML LatexLabel intFunctionLatexLabel, intAnswerLatexLabel;
@@ -34,6 +36,12 @@ public class ChapterFourController {
 
         intMethodType.getItems().addAll("Trapezoidal", "Simpson 1/3", "Simpson 3/8",
                 "Romberg", "Gauss Legendre", "Customized Simpson");
+        intMethodType.valueProperty().addListener(((observable, oldValue, newValue) -> {
+            if (newValue.equals("Romberg") || newValue.equals("Gauss Legendre"))
+                intHLabel.setText("n = ");
+            else
+                intHLabel.setText("h = ");
+        }));
         intFunctionTextField.textProperty().addListener((observable, oldValue, newValue) -> {
             intFunctionLatexLabel.setLatex(parser.latex(newValue));
         });
@@ -41,15 +49,13 @@ public class ChapterFourController {
     }
 
     public void callIntegral() {
-        double start = Double.parseDouble(intStartTextField.getText()),
-                end = Double.parseDouble(intEndTextField.getText()),
-                h = Double.parseDouble(intHTextField.getText());
-        double n = (end - start) / h;
+        int n = Integer.parseInt(intHTextField.getText());
         MatlabStruct args = new MatlabStruct(
                 new MatlabStruct.Pair<>("f", intFunctionTextField.getText()),
                 new MatlabStruct.Pair<>("start", Double.parseDouble(intStartTextField.getText())),
                 new MatlabStruct.Pair<>("end", Double.parseDouble(intEndTextField.getText())),
                 new MatlabStruct.Pair<>("h", Double.parseDouble(intHTextField.getText())),
+                new MatlabStruct.Pair<>("h", n),
                 new MatlabStruct.Pair<>("method", intMethodType.getSelectionModel().getSelectedIndex()),
                 new MatlabStruct.Pair<>("plotPath", getClass().getResource("/matlab").getPath() + "/plot.jpg")
         );
@@ -60,21 +66,21 @@ public class ChapterFourController {
             new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
         }
 
-        double[] error = (double[]) res.get("error");
+        double[] error = res.get("error");
 
         if (error[0] == 0)
             intPlotView.setImage(new Image(this.getClass().getResourceAsStream("/matlab/plot.jpg")));
 
         if (intMethodType.getSelectionModel().getSelectedIndex() <= 2 || intMethodType.getSelectionModel().getSelectedIndex() == 5) {
-            String result = (String) res.get("result");
+            String result = res.get("result");
             intAnswerLatexLabel.setLatex(result);
         } else if (intMethodType.getValue().equals("Romberg")) {
-            double[] result = (double[]) res.get("result");
+            double[] result = res.get("result");
             // TODO: Create latex table from result
-            double[][] table = new double[(int)n][(int)n + 1];
+            double[][] table = new double[n][n + 1];
 
             for (int i = 0; i < result.length; i ++)
-                table[i % ((int) n)][i / ((int) n)] = result[i];
+                table[i % n][i / n] = result[i];
             intAnswerLatexLabel.setLatex(ChapterSixController.makeTable(table));
         }
     }
--- app/src/resources/matlab/chapter-4/integrate.m
+++ app/src/resources/matlab/chapter-4/integrate.m
@@ -1,41 +1,42 @@
-function [result, error] = integrate(func, a, b, h, method, addr)
+function [result, error] = integrate(func, a, b, h, n, method, addr)
 
 func = strcat('@(x)', func);
 f = str2func(func);
 
-n = (b - a) / h;
-xs = [a:h:b];
-ys = arrayfun(f, xs);
-
 error = 0;
 
-if (method == 0)
-    intValue = trapezoidal(xs, ys, h);
-elseif (method == 1)
-    if (mod(n, 2) == 1)
-        error = 1;
-        result = '\text{N must be even!}';
-    else
-        intValue = simpsonOneThird(xs, ys, h);
+if (method <= 2 || method == 5)
+    n = (b - a) / h;
+    xs = [a:h:b];
+    ys = arrayfun(f, xs);
+    if (method == 0)
+        intValue = trapezoidal(xs, ys, h);
+    elseif (method == 1)
+        if (mod(n, 2) == 1)
+            error = 1;
+            result = '\text{N must be even!}';
+        else
+            intValue = simpsonOneThird(xs, ys, h);
+        end
+    elseif (method == 2)
+        if (mod(n, 3) ~= 0)
+            error = 1;
+            result = '\text{N must be a multiple of 3}';
+        else
+            intValue = simpsonThreeEight(xs, ys, h);
+        end
+    elseif (method == 5)
+        intValue = customSimpson(xs, ys, h);
     end
-elseif (method == 2)
-    if (mod(n, 3) ~= 0)
-        error = 1;
-        result = '\text{N must be a multiple of 3}';
-    else
-        intValue = simpsonThreeEight(xs, ys, h);
-    end
-elseif (method == 5)
-    intValue = customSimpson(xs, ys, h);
-elseif (method == 3)
-    result = romberg(f, a, b, n);
-end
 
-% create result string if no errors occured.
-if (error == 0)
-    if (method <= 2 || method == 5)
+    % create result string if no errors occured.
+    if (error == 0)
         result = strcat(mat2str(xs), '\\', mat2str(ys), '\\', num2str(intValue));
     end
+elseif (method == 3)
+    result = romberg(f, a, b, n);
+elseif (method == 4)
+
 end
 
 % plot
