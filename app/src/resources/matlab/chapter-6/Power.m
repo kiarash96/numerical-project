bbd5fc5f2588a28f57d318027e598e03716b5c90
Kiarash Korki <kiarash.96@gmail.com>
2017-01-05 23:08:43 +0330

    add Eigenvalue matlab codes

---
+++ app/src/resources/matlab/chapter-6/Power.m
@@ -0,0 +1,19 @@
+function [fail, message,history , final ]=Power(A, x, t)
+fail = 0;
+message = '';
+history = 0;
+m=0;
+n=length(x);
+y_final=x;
+i = 1;
+history = zeros ( x, t);
+while(i<=t)
+     history(:, i)=y_final; %// Change - Save old eigenvector
+     y_final=A*y_final;
+     m=max(y_final);
+     y_final=y_final/m;
+     i = i+1;
+end
+final = y_final;
+end
+
